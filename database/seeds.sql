d6114a216065ae3a6c2c9d4f497907d7ab776b34
Karolina Redden <dubajkaro@gmail.com>
2022-10-28 19:30:39 +0000

    seed db not finished

---
+++ database/seeds.sql
@@ -0,0 +1,23 @@
+INSERT INTO categories (name)
+VALUES 
+('Breakfast'),
+('Fit'),
+('Dessert'),
+('Lunch');
+
+INSERT INTO ingredients (name)
+VALUES
+('Avocado'),
+('Egg'),
+('Wrap');
+
+
+INSERT INTO recipes (title, description, category_id, ingredient_id, picture)
+VALUES
+('Avocado and Egg Breakfast Burrito', 'awesome', 1, 1, 'https://www.allrecipes.com/thmb/EOv7sxOy5owhxlll9Qb9bT6MREY=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/9198492-e01d21c2706e4d54a06ac9c7d71b7ee8.jpg');
+-- ('Breakfast'),
+-- ('Breakfast'),
+-- ('Breakfast'),
+-- ('Breakfast'),
+-- ('Breakfast');
+
--- database/tables.sql
+++ database/tables.sql
@@ -1,6 +1,6 @@
+DROP TABLE IF EXISTS categories CASCADE;
 DROP TABLE IF EXISTS ingredients CASCADE;
 DROP TABLE IF EXISTS recipes CASCADE;
-DROP TABLE IF EXISTS categories CASCADE;
 
 
 CREATE TABLE categories (
@@ -8,17 +8,20 @@ CREATE TABLE categories (
   name VARCHAR(255) NOT NULL
 );
 
+CREATE TABLE ingredients (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL
+  -- recipe_id INTEGER REFERENCES recipes(id) ON DELETE CASCADE
+);
+
 CREATE TABLE recipes (
   id SERIAL PRIMARY KEY NOT NULL,
   title VARCHAR(255) NOT NULL,
   description VARCHAR(255) NOT NULL,
-  ingredients VARCHAR(255) NOT NULL,
-  cook_time TIME,
-  category_id INTEGER REFERENCES categories(id) ON DELETE CASCADE
+  category_id INTEGER REFERENCES categories(id) ON DELETE CASCADE,
+  ingredient_id INTEGER REFERENCES ingredients(id) ON DELETE CASCADE,
+  -- video_instruction VARCHAR(255),
+  picture VARCHAR(255)
+  -- cook_time TIME
 );
 
-CREATE TABLE ingredients (
-  id SERIAL PRIMARY KEY NOT NULL,
-  item VARCHAR(255) NOT NULL,
-  recipe_id INTEGER REFERENCES recipes(id) ON DELETE CASCADE
-)
