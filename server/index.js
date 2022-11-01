fed763c20c99ce13c1f172e2d44b93c73662c567
Karolina Redden <dubajkaro@gmail.com>
2022-11-01 17:00:17 +0000

    add route to read all the recipes

--- server/index.js
+++ server/index.js
@@ -9,3 +9,14 @@ const PORT = 8001;
 app.listen(PORT, () => {
   console.log(`App runs on port ${PORT}`);
 });
+
+app.get('/recipes', async (req, res) => {
+  try {
+    const getAllrecipes = await pool.query(
+      'SELECT categories.name AS category, recipes.id, recipes.title, recipes.description, recipes.category_id, recipes.picture FROM recipes JOIN categories ON recipes.category_id = categories.id'
+    );
+    res.json(getAllrecipes.rows);
+  } catch (err) {
+    console.error(err.message)
+  }
+});
