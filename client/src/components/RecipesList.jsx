06b671608b4c188c39f2257d24acd690f0fcd338
Karolina Redden <dubajkaro@gmail.com>
2022-11-01 16:34:57 +0000

    add table

--- client/src/components/RecipesList.jsx
+++ client/src/components/RecipesList.jsx
@@ -2,6 +2,24 @@ export default function RecipesList() {
   return (
     <div>
       Recipes List:
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Edit</th>
+            <th>Remove</th>
+          </tr>
+        </thead>
+        <tbody>
+          <td>pic</td>
+          <td>not</td>
+          <td>here</td>
+          <td>Edit button</td>
+          <td>Delete button</td>
+        </tbody>
+      </table>
     </div>
-  )
+  );
 }
