96eaa77e789e476299c0e06837b5e49072bc94a4
Karolina Redden <dubajkaro@gmail.com>
2022-11-02 17:43:41 +0000

    add a table to list categories

--- client/src/components/CategoriesList.jsx
+++ client/src/components/CategoriesList.jsx
@@ -1,5 +1,20 @@
 export default function CategoriesList() {
   return (
-    <div> Categories list: </div>
-  )
-};
+    <div>
+      {" "}
+      Categories list table
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Category name</th>
+          </tr>
+          <tr>
+            <td>Number</td>
+            <td>Name</td>
+          </tr>
+        </thead>
+      </table>
+    </div>
+  );
+}
