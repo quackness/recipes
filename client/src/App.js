127690ec801845b674b1dee304c787644d774eed
Karolina Redden <dubajkaro@gmail.com>
2022-11-02 16:46:41 +0000

    add useState and useEffect to display the list of recipes

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,24 @@ import Newcategory from './components/NewCategory';
 import NewRecipe from './components/NewRecipe';
 import NewIngredient from './components/NewIngredient';
 import RecipesList from './components/RecipesList';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
-
+ 
 function App() {
+  const [recipes, setRecipes] = useState([]);
+  const [ingredients, setIngredients] = useState([]);
+
+  useEffect(() => {
+    axios.get(`http://localhost:8001/recipes`)
+      .then(function(res) {
+        setRecipes([...res.data])
+      })
+  }, [])
+
   return (
     <div className="App">
-      <RecipesList />
+      <RecipesList recipes={recipes}/>
       <CategoriesList />
       <IngredientsList />
       <NewRecipe />
