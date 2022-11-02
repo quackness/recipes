2b3d9948cfff1d7a9438d3207c2d8183b209856e
Karolina Redden <dubajkaro@gmail.com>
2022-11-02 18:25:17 +0000

    add bootstrap buttons

--- client/src/components/CategoriesList.jsx
+++ client/src/components/CategoriesList.jsx
@@ -18,8 +18,8 @@ export default function CategoriesList(props) {
             <tr key={category.id}>
               <td>{category.id}</td>
               <td>{category.name}</td>
-              <td>Edit button</td>
-              <td>Delete button</td>
+              <td><button type="button" className="btn btn-success">Edit</button></td>
+              <td><button type="button" className="btn btn-danger">Delete</button></td>
             </tr>
           )}
         </tbody>
