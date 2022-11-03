3b07d1a3b14d18861e8f1a8875475ba8a1e72240
Karolina Redden <dubajkaro@gmail.com>
2022-11-03 16:46:41 +0000

    add onClick and axios for delete action

--- client/src/components/CategoriesList.jsx
+++ client/src/components/CategoriesList.jsx
@@ -1,5 +1,38 @@
+import axios from "axios";
+
 export default function CategoriesList(props) {
-  const { categories } = props;
+  const { categories, setCategories } = props;
+
+  // const deleteCategory = function (id) {
+  //   return axios.delete(`http://localhost:8001/category_delete/${id}`)
+  //       .then(res => {
+  //       setCategories(categories.filter(category => category.id !== id));
+  //       console.log("Category deleted id:", id);
+  //     });
+  // };
+
+  const deleteCategory = function(id) {
+    return axios.delete(`http://localhost:8001/category_delete/${id}`)
+      .then(res => {
+        setCategories(categories.filter(category => category.id !== id))
+        console.log("Category deleted id:", id)
+      })
+  }
+
+  // const deleteRecipe = function(id) {
+  //   return axios.delete(`http://localhost:8001/recipes_delete/${id}`)
+  //     .then(res => {
+  //       setRecipes(recipes.filter(recipe => recipe.id !== id))
+  //       console.log("Recipe deleted id:", id)
+  //     })
+  // }
+
+
+
+
+
+
+
   return (
     <div>
       Categories list table
@@ -14,14 +47,26 @@ export default function CategoriesList(props) {
         </thead>
 
         <tbody>
-          {categories.map(category => 
+          {categories.map((category) => (
             <tr key={category.id}>
               <td>{category.id}</td>
               <td>{category.name}</td>
-              <td><button type="button" className="btn btn-success">Edit</button></td>
-              <td><button type="button" className="btn btn-danger">Delete</button></td>
+              <td>
+                <button type="button" className="btn btn-success">
+                  Edit
+                </button>
+              </td>
+              <td>
+                <button
+                  type="button"
+                  className="btn btn-danger"
+                  onClick={() => deleteCategory(category.id)}
+                >
+                  Delete
+                </button>
+              </td>
             </tr>
-          )}
+          ))}
         </tbody>
       </table>
     </div>
